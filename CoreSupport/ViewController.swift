5bf5ac3eb3e4ae64e7a070391ef9864c0dfe7101
Jayme Rutkoski <jaymerut@gmail.com>
2020-07-05 09:07:20 -0700

    Initial Commit

---
+++ CoreSupport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreSupport
+//
+//  Created by Jayme Rutkoski on 7/5/20.
+//  Copyright © 2020 coresupport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
