1176fe77a14e58dd03ae224b9c37ad10c62ec3f1
Jayme Rutkoski <jaymerut@gmail.com>
2020-07-06 09:25:34 -0700

    Added Services & Opportunities View 	- Included a CollectionView in BaseViewController 	- Updated with section info

--- CoreSupport/ViewControllers/BaseViewController.swift
+++ CoreSupport/ViewControllers/BaseViewController.swift
@@ -14,6 +14,7 @@ class BaseViewController: UIViewController {
     
     
     // MARK: - Variables
+    var collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
     public var imageViewBackground = UIImageView(frame: CGRect.zero)
     
     // MARK: - Initialization
@@ -35,8 +36,15 @@ class BaseViewController: UIViewController {
         
         customInitBaseViewController()
     }
-    
-    
+    /*
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if #available(iOS 13.0, *) {
+            return .lightContent
+        } else {
+            return .lightContent
+        }
+    }
+    */
     
     // MARK: - UIViewController Lifecycle Methods
     override func viewDidLoad() {
@@ -59,13 +67,29 @@ class BaseViewController: UIViewController {
     
     // MARK: - Private API
     private func setupBaseViewController() {
+        self.view.backgroundColor = UIColor(hex: "#D3E4CDFF")
+        // Scroll View
+        collectionView.backgroundColor = UIColor(hex: "#D3E4CDFF")
+        self.view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { (make) -> Void in
+            if #available(iOS 11.0, *) {
+                make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            } else {
+                make.top.equalTo(self.view.snp.top)
+            }
+            make.left.equalTo(self.view.snp.left)
+            make.right.equalTo(self.view.snp.right)
+            make.bottom.equalTo(self.view.snp.bottom)
+        }
         
         // Image View Background
         imageViewBackground.image = UIImage(named: "coresupport_background")
         imageViewBackground.contentMode = UIView.ContentMode.scaleAspectFill
-        self.view.addSubview(imageViewBackground)
+        collectionView.addSubview(imageViewBackground)
         imageViewBackground.snp.makeConstraints { (make) -> Void in
-            make.edges.equalTo(self.view)
+            make.top.bottom.equalTo(self.collectionView)
+            make.left.right.equalTo(self.view)
+            make.height.greaterThanOrEqualTo(self.collectionView)
         }
          
     }
